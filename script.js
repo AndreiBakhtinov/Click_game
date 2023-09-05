f5abe982c46481aab3bd8cc9a07879af85866ba0
Andrei Bakhtinov <root@ab88.pro>
2023-09-05 23:49:56 +0400

    update

--- script.js
+++ script.js
@@ -1,52 +1,62 @@
 function start_game() {
-    object.classList.toggle('start');
+  document.title = `Score ${score}`;
+  object.classList.toggle("is-moving");
 }
 
 function finish_game() {
-    object.classList.remove('start');
-    alert(`You lose. Score ${score}`);
+  alert(`You lose. Score ${score}`);
+  score = 0;
 }
 
 function hit() {
   score++;
   document.title = `Score ${score}`;
 
-  object.classList.remove('start');
+  object.classList.remove("is-moving");
   void object.offsetWidth; // магия
-  object.classList.add('start');
-
-  let random_offset = Math.random() * 340;
-  object.style.left = `${random_offset}px`;
+  object.classList.add("is-moving");
 
   change_object_background();
+  change_object_offset();
+  change_object_size();
 
   hit_sound.currentTime = 0;
-  hit_sound.play(); // play `hit` sound
+  hit_sound.play(); // play `miss` sound
+}
+
+function change_object_size() {
+  const random_size = 20 + Math.random() * 40;
+  object.style.width = `${random_size}px`;
+}
+
+function change_object_offset() {
+  const random_offset = Math.random() * 340;
+  object.style.left = `${random_offset}px`;
+}
+
+function change_object_background() {
+  const colors = ["red", "gold", "blue", "orange", "purple"];
+  const random_index = Math.floor(Math.random() * colors.length);
+  object.style.background = colors[random_index];
 }
 
 function miss(event) {
   if (event.target.id == "area") {
     score--;
-
-    if (score <= 0) {
-      finish_game();
-    }
-
     document.title = `Score ${score}`;
 
     miss_sound.currentTime = 0;
     miss_sound.play(); // play `miss` sound
+
+    if (score <= 0) {
+      finish_game();
+    }
   }
 }
 
-function change_object_background() {
-    const colors = ['purple', 'gold', 'orange', 'blue', 'red'];
-    const random_index = Math.floor(Math.random() * colors.length);
-    object.style.background = colors[random_index];
-}
-
 let score = 0;
-let object = document.querySelector('#object');
+let object = document.querySelector("#object");
 
-const hit_sound = new Audio('sounds/hit.wav');
-const miss_sound = new Audio('sounds/miss.wav');
+// подключить звуковые дорожки
+const hit_sound = new Audio("sounds/hit.wav");
+const miss_sound = new Audio("sounds/miss.wav");
